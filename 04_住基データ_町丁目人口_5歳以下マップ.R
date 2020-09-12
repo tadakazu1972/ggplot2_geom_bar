9d3ba8f483ca104f7c24f42f3b568d9f6561bc81
tadakazu1972 <tadakazu1972@gmail.com>
2020-09-12 11:22:32 +0900

    add kita

---
+++ 04_住基データ_町丁目人口_5歳以下マップ.R
@@ -0,0 +1,57 @@
+#ライブラリ
+library(dplyr)
+library(sf)
+library(readr)
+library(RColorBrewer)
+library(classInt)
+
+#作業ディレクトリ
+setwd("~/Desktop/ggplot2_geom_bar/")
+
+#シェープファイル読込
+#eStatから取得 .dbf .prj .shxも合わせて作業ディレクトリ下部のshapeフォルダに入れておくべし。
+shape <- st_read(dsn = "~/Desktop/ggplot2_geom_bar/shape/", layer = "h27ka27127")
+
+#住民基本台帳csv読込
+data1 <- read_csv("./jyuki_kita_202003.csv")
+
+#男女別が「計」のデータだけ抽出
+data2 <- data1 %>% filter(data1$男女別=="計")
+
+#5歳以下を足し算して列を追加
+child <- apply(data2[,8:13], 1, sum)
+data2 <- data2 %>% mutate("5歳以下" = child )
+
+#shapeファイルとcsvを結合
+data <- left_join(shape, data2, by=c("MOJI"="町丁目名"))
+
+#カラム名取得
+column = colnames(data)
+kuname="北区"
+
+#######################################################
+#pngファイル版
+{
+  quartz(type="png", file=sprintf(paste(kuname, "_住基202003_5歳以下.png", sep="")), dpi=144, bg="white")
+
+  par(new=TRUE, mex="0.2", family="HiraKakuProN-W3", xpd=TRUE, xaxt="n")
+
+  #色を固定する場合の記述
+  col_km <- data[[column[144]]] %>% classIntervals(., 10, style="fixed", fixedBreaks=c(10,25,50,75,100,200,300,400,500,1000,max(.))) %>% findColours(.,pal=brewer.pal(10,"Greens"))
+
+  #境界
+  plot(st_geometry(shape[7]), col=col_km, main=paste(kuname, "　人口　5歳以下 (住民基本台帳 2020年3月末現在)", sep=""))
+
+  #町丁目名
+  text(st_coordinates(shape %>% st_centroid)[,1], st_coordinates(shape %>% st_centroid)[,2]+0.0005, labels=shape$MOJI, cex=0.4)
+
+  #数値
+  text(st_coordinates(shape %>% st_centroid)[,1], st_coordinates(shape %>% st_centroid)[,2]-0.0003, labels=data[[column[144]]], cex=0.5)
+  dev.off()
+}
+
+#色はK-meansで自動計算する場合の記述
+col_km <- data[[column[144]]] %>% classIntervals(., 10, style="kmeans") %>% findColours(.,pal=brewer.pal(10,"Greens"))
+
+#色を固定する場合の記述
+#col_km <- data[[column[144]]] %>% classIntervals(., 10, style="fixed", fixedBreaks=c(100,200,300,400,500,600,700,800,900,1000,max(.))) %>% findColours(.,pal=brewer.pal(10,"Greens"))
